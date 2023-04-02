41bf459546cbc8368bc96beb33298cb24acc95fa
Anna Samoilova <samoilova0299@gmail.com>
2023-04-02 17:23:01 +0300

    add header hero

---
+++ src/swiper-hero.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper(".image-slider", {
+    // Optional parameters
+  
+    loop: true,
+  
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    //   autoplay: {
+    //     delay: 1000,
+    //     disableOnInteraction: false,
+        stopOnLastSlide: false,
+  
+    //   },
+    speed: 1500,
+    //   breakpoints: {
+    //     // when window width is >= 320px
+    //     320: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 20
+    //     },
+    //     // when window width is >= 480px
+    //     480: {
+    //       slidesPerView: 3,
+    //       spaceBetween: 30
+    //     },
+    //     // when window width is >= 640px
+    //     640: {
+    //       slidesPerView: 4,
+    //       spaceBetween: 40
+    //     }
+    //   }
+  });
